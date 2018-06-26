6dd3abca7604ce2125976064c4fca015404a1b4f
israelhz <israel.leal25@gmail.com>
2018-06-25 22:59:34 -0500

    Adds ApiConstraints model with its specs

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
-      
+    scope module: :v1,
+                  constraints: ApiConstraints.new(version: 1, default: true) do
+
     end
   end
 end
