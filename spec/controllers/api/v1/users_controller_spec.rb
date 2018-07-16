acbac354fefb600d93e335f8ee371fb394f797ff
israelhz <israel.leal25@gmail.com>
2018-07-16 11:06:01 -0500

    Adds session controller create action

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -85,7 +85,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
         expect(user_response).to have_key(:errors)
       end
 
-      it "renders the json errors on whye the user could not be created" do
+      it "renders the json errors on why the user could not be created" do
         user_response = json_response
         expect(user_response[:errors][:email]).to include "is invalid"
       end
