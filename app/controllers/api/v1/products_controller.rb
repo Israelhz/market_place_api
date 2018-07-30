b4f87cf79918cd7ba4ad3a47d2a2100ca9760270
israelhz <israel.leal25@gmail.com>
2018-07-30 15:31:31 -0500

    Adds search class method to filter products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,12 +3,7 @@ class Api::V1::ProductsController < ApplicationController
   respond_to :json
 
   def index
-    products = if params[:product_ids].present?
-                 Product.find(params[:product_ids])
-               else 
-                 Product.all
-               end
-    respond_with products
+    respond_with Product.search(params)
   end
 
   def show
